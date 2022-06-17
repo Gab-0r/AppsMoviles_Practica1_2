b46727ab0300287e8a76d1a1dabecf0508d1a19a
Juan Gabriel Orozco <jgabriel.orozco@udea.edu.co>
2022-06-16 20:21:37 -0500

    Se implementa el ViewModel y LiveData exitosamente.

--- app/src/main/java/com/example/appsmoviles_practica1_2/ui/main/MainActivity.kt
+++ app/src/main/java/com/example/appsmoviles_practica1_2/ui/main/MainActivity.kt
@@ -2,16 +2,14 @@ package com.example.appsmoviles_practica1_2.ui.main
 
 import android.app.Activity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.ViewModelProvider
 import com.example.appsmoviles_practica1_2.databinding.ActivityMainBinding
 
-class MainActivity : Activity() {
-    //COP-USD-EUR
-    val cop_conv = floatArrayOf(1f, 0.00026f, 0.00025f) //Arreglo de conversiones de COP a COP USD EUR
-    val usd_conv = floatArrayOf(3781f, 1f, 0.93f) //Arreglo de conversiones de USD a COP USD EUR
-    val eur_conv = floatArrayOf(4053f, 1.07f, 1f) //Arreglo de conversiones de EUR a COP USD EUR
-
+class MainActivity : AppCompatActivity() {
 
     private lateinit var mainBinding: ActivityMainBinding
+    private lateinit var mainViewModel: MainViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -19,23 +17,14 @@ class MainActivity : Activity() {
         val view = mainBinding.root
         setContentView(view)
 
-        with(mainBinding){
+        mainViewModel = ViewModelProvider(this)[MainViewModel::class.java]
+        mainViewModel.convert_resultDone.observe(this){
+            mainBinding.textViewCurrencyTo.text = it.toString()
+        }
 
+        with(mainBinding){
             convertButton.setOnClickListener{
-                var factConv = floatArrayOf(0f,0f,0f)
-                var moneyAmount = TextInputEditTextCurrencyFrom.text.toString().toInt()
-                var CurrencyFrom_ = spCurrencyFrom.selectedItemPosition //Selecciona la moneda a convertir
-                var CurrencyTo_ = spCurrencyTo.selectedItemPosition //Selecciona la moneda a la cual se quiere llegar
-                when(CurrencyFrom_){ //Se selecciona el vector de factores de conversion en base a la opción seleccionada
-                    0 ->    factConv = cop_conv.copyOf()
-                    1 ->    factConv = usd_conv.copyOf()
-                    2 ->    factConv = eur_conv.copyOf()
-                    else ->{
-                        factConv = floatArrayOf(0f,0f,0f)
-                    }
-                }
-                var convertResult = moneyAmount * factConv.get(CurrencyTo_)
-                textViewCurrencyTo.text = convertResult.toString()
+                mainViewModel.currency_conversion(spCurrencyFrom.selectedItemPosition, spCurrencyTo.selectedItemPosition, TextInputEditTextCurrencyFrom.text.toString().toInt())
             }
         }
     }
---
+++ app/src/main/java/com/example/appsmoviles_practica1_2/ui/main/MainViewModel.kt
@@ -0,0 +1,29 @@
+package com.example.appsmoviles_practica1_2.ui.main
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class MainViewModel : ViewModel() {
+
+    private val cop_conv = floatArrayOf(1f, 0.00026f, 0.00025f)
+    private val usd_conv = floatArrayOf(3781f, 1f, 0.93f)
+    private val eur_conv = floatArrayOf(4053f, 1.07f, 1f)
+    private var factConv = floatArrayOf(0f,0f,0f)
+    private var convert_result : MutableLiveData<Float> = MutableLiveData()
+    val convert_resultDone: LiveData<Float> = convert_result
+
+  fun currency_conversion(currencyfrom: Int, currencyto: Int, amount: Int){
+        when(currencyfrom){
+            0 -> factConv = cop_conv.copyOf()
+            1 -> factConv = usd_conv.copyOf()
+            2 -> factConv = eur_conv.copyOf()
+            else ->{
+                factConv = floatArrayOf(0f,0f,0f)
+            }
+        }
+        convert_result.value = amount * factConv.get(currencyto)
+    }
+
+
+}
